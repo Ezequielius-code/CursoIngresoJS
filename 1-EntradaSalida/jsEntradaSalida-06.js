dd6aadc9532bfe0841a4fd0bf10e082a9fca7db0
ez-line <74914562+EZ-LINE@users.noreply.github.com>
2021-02-01 11:47:47 -0300

    Sumar 2 enteros

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,19 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let number1;
+	let number2;
+	let result;
+
+	number1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	number2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	result = number1 + number2;
+
+	alert("El resultado de la suma es " + result);
+
+	document.getElementById("txtIdNumeroUno").value = "";
+	document.getElementById("txtIdNumeroDos").value = "";
+
 }
 
