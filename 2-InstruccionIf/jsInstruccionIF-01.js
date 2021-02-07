5145eb74cf81250cab109c1ba166f15267d09b86
Ezequielius-code <bruttoezequiel@gmail.com>
2021-02-07 09:55:43 -0300

    1,2

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,14 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+	var age;
+
+	age = document.getElementById('txtIdEdad').value;
+	age = parseInt(age);
+
+	if (age == 15)
+	{
+		alert("Niña bonita");
+	}
 
 }//FIN DE LA FUNCIÓN
