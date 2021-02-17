5ceb249f8dec9c85ffd41ba2f1e544cada7f020c
Ezequielius-code <bruttoezequiel@gmail.com>
2021-02-17 01:26:22 -0300

    TP´s new update

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -8,7 +8,340 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
+ //Brutto, Ezequiel Andrés
+/*function CalcularPrecio () 
+{
+ 	var lightBulbs;
+ 	var brand;
+ 	var price;
+ 	var discount;
+ 	var finalPrice;
+ 	var grossIncome;
+ 	var message;
+ 	
+ 	lightBulbs = document.getElementById('txtIdCantidad').value;
+ 	lightBulbs = parseInt(lightBulbs);
+
+ 	brand = document.getElementById('Marca').value;
+
+ 	price = lightBulbs * 35;
+ 	discount = 0;
+
+
+ 	if (lightBulbs > 5) 
+ 	{
+ 		discount = 50;
+ 	}
+ 	else 
+ 	{
+ 		if (lightBulbs == 5) 
+ 		{
+ 			if (brand == "ArgentinaLuz") 
+ 			{
+ 				discount = 40;
+ 			}
+ 			else 
+ 			{
+ 				discount = 30;
+ 			}
+ 		}
+ 		else 
+ 		{
+ 			if (lightBulbs == 4) 
+ 			{
+ 				if (brand == "ArgentinaLuz" || brand == "FelipeLamparas") 
+ 				{
+ 					discount = 25;
+ 				}
+ 				else
+ 				{
+ 					discount = 20;
+ 				}
+ 			}
+ 			else 
+ 			{
+ 				if (lightBulbs == 3) 
+ 				{
+ 					if (brand == "ArgentinaLuz") 
+ 					{
+ 						discount = 15;
+ 					}
+ 					else 
+ 					{
+ 						if (brand == "FelipeLamparas") 
+ 					    {
+ 						discount = 10;
+ 					    }
+ 					    else 
+ 					    {
+ 					    	discount = 5;
+ 					    }
+ 					}
+ 				}
+ 			}
+ 		}
+ 	}
+
+ 	finalPrice = price - (price * discount / 100);
+
+ 	if (discount > 0 && finalPrice > 120) 
+ 	{
+ 		grossIncome = finalPrice * 10/100;  
+ 		finalPrice = finalPrice + grossIncome;
+	    alert("Usted pagó $ " + grossIncome + ".- de IIBB");
+ 	}
+
+ 	message = "El precio final de su compra es de $ " + finalPrice + ".-" ;
+    
+    document.getElementById('txtIdprecioDescuento').value = message;
+}*/	
+
+//Brutto, Ezequiel Andrés.
+/*function CalcularPrecio ()
+{
+	var lightBulbs;
+ 	var brand;
+ 	var price;
+ 	var discount;
+ 	var finalPrice;
+ 	var grossIncome;
+ 	var message;
+ 	
+ 	lightBulbs = document.getElementById('txtIdCantidad').value;
+ 	lightBulbs = parseInt(lightBulbs);
+
+ 	brand = document.getElementById('Marca').value;
+
+ 	price = lightBulbs * 35;
+ 	
+    switch(lightBulbs)
+ 	{
+ 		case 1:
+ 		case 2:
+ 		    discount = 0;
+ 		    break;
+ 		case 3:
+ 		    switch(brand)
+ 		    {
+ 		    	case "ArgentinaLuz":
+ 		    	    discount = 15;
+ 		    	    break;
+ 		    	case "FelipeLamparas":
+ 		    	    discount = 10;
+ 		    	    break;
+ 		    	default:
+ 		    	    discount = 5;
+ 		    	    break;
+ 		    }
+ 		break;
+ 		case 4:
+ 		    switch(brand)
+ 		    {
+ 		    	case "ArgentinaLuz":
+ 		    	case "FelipeLamparas":
+ 		    	    discount = 25;
+ 		    	    break;
+ 		    	default:
+ 		    	    discount = 20;
+ 		    	    break;
+ 		    }
+ 		break;
+ 		case 5:
+ 		    switch(brand)
+ 		    {
+ 		    	case "ArgentinaLuz":
+ 		    	    discount = 40;
+ 		    	    break;
+ 		    	default:
+ 		    	    discount = 30;
+ 		    	    break;
+ 		    }
+ 		break;
+ 		default:
+ 		    discount = 50;
+ 		    break;
+
+ 	}
+
+    finalPrice = price - (price * discount / 100);
+
+ 	if (discount > 0 && finalPrice > 120) 
+ 	{
+ 		grossIncome = finalPrice * 10/100;  
+ 		finalPrice = finalPrice + grossIncome;
+	    alert("Usted pagó $ " + grossIncome + ".- de IIBB");
+ 	}
+
+ 	message = "El precio final de su compra es de $ " + finalPrice + ".-" ;
+    
+    document.getElementById('txtIdprecioDescuento').value = message;
+}*/
+
+//Brutto, Ezequiel Andrés.
+/*function CalcularPrecio ()
+{
+	var lightBulbs;
+ 	var brand;
+ 	var price;
+ 	var discount;
+ 	var finalPrice;
+ 	var grossIncome;
+ 	var message;
+ 	
+ 	lightBulbs = document.getElementById('txtIdCantidad').value;
+ 	lightBulbs = parseInt(lightBulbs);
+
+ 	brand = document.getElementById('Marca').value;
+
+ 	price = lightBulbs * 35;
+ 	
+    switch(lightBulbs)
+ 	{
+ 		case 1:
+ 		case 2:
+ 		    discount = 0;
+ 		    break;
+ 		case 3:
+ 		   if (brand == "ArgentinaLuz") 
+ 			{
+ 				discount = 15;
+ 			}
+ 			else 
+ 			{
+ 				if (brand == "FelipeLamparas") 
+ 				{
+ 					discount = 10;
+ 				}
+ 			    else 
+ 			    {
+ 					discount = 5;
+ 				}
+ 			}
+ 		break;
+ 		case 4:
+ 		    if (brand == "ArgentinaLuz" || brand == "FelipeLamparas") 
+ 			{
+ 				discount = 25;
+ 			}
+ 			else
+ 			{
+ 				discount = 20;
+ 			}
+ 		break;
+ 		case 5:
+ 		    if (brand == "ArgentinaLuz") 
+ 			{
+ 				discount = 40;
+ 			}
+ 			else 
+ 			{
+ 				discount = 30;
+ 			}
+ 		break;
+ 		default:
+ 		    discount = 50;
+ 		    break;
+
+ 	}
+
+    finalPrice = price - (price * discount / 100);
+
+ 	if (discount > 0 && finalPrice > 120) 
+ 	{
+ 		grossIncome = finalPrice * 10/100;  
+ 		finalPrice = finalPrice + grossIncome;
+	    alert("Usted pagó $ " + grossIncome + ".- de IIBB");
+ 	}
+
+ 	message = "El precio final de su compra es de $ " + finalPrice + ".-" ;
+    
+    document.getElementById('txtIdprecioDescuento').value = message;
+}*/
+
+//Brutto, Ezequiel Andrés
 function CalcularPrecio () 
 {
+ 	var lightBulbs;
+ 	var brand;
+ 	var price;
+ 	var discount;
+ 	var finalPrice;
+ 	var grossIncome;
+ 	var message;
  	
+ 	lightBulbs = document.getElementById('txtIdCantidad').value;
+ 	lightBulbs = parseInt(lightBulbs);
+
+ 	brand = document.getElementById('Marca').value;
+
+ 	price = lightBulbs * 35;
+ 	discount = 0;
+
+
+ 	if (lightBulbs > 5) 
+ 	{
+ 		discount = 50;
+ 	}
+ 	else 
+ 	{
+ 		if (lightBulbs == 5) 
+ 		{
+ 			switch(brand)
+ 		    {
+ 		    	case "ArgentinaLuz":
+ 		    	    discount = 40;
+ 		    	    break;
+ 		    	default:
+ 		    	    discount = 30;
+ 		    	    break;
+ 		    }
+ 		}
+ 		else 
+ 		{
+ 			if (lightBulbs == 4) 
+ 			{
+ 				switch(brand)
+ 		        {
+ 		    	    case "ArgentinaLuz":
+ 		    	    case "FelipeLamparas":
+ 		    	        discount = 25;
+ 		    	        break;
+ 		    	    default:
+ 		    	        discount = 20;
+ 		    	        break;
+ 		        }
+ 		    }
+ 			else 
+ 			{
+ 				if (lightBulbs == 3) 
+ 				{
+ 					switch(brand)
+ 		            {
+ 		    	        case "ArgentinaLuz":
+ 		    	            discount = 15;
+ 		    	            break;
+ 		    	        case "FelipeLamparas":
+ 		    	            discount = 10;
+ 		    	            break;
+ 		    	        default:
+ 		    	            discount = 5;
+ 		    	            break;
+ 		            }  
+ 			    } 				 			
+ 			}
+ 		}
+ 	}
+
+ 	finalPrice = price - (price * discount / 100);
+
+ 	if (discount > 0 && finalPrice > 120) 
+ 	{
+ 		grossIncome = finalPrice * 10/100;  
+ 		finalPrice = finalPrice + grossIncome;
+	    alert("Usted pagó $ " + grossIncome + ".- de IIBB");
+ 	}
+
+ 	message = "El precio final de su compra es de $ " + finalPrice + ".-" ;
+    
+    document.getElementById('txtIdprecioDescuento').value = message;
 }
--- 8-TPs/12-jsRisingBTL.js
+++ 8-TPs/12-jsRisingBTL.js
@@ -7,13 +7,111 @@ D.	Sueldo bruto, no menor a 8000.
 E.	Número de legajo, numérico de 4 cifras, sin ceros a la izquierda.
 F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.
  */
+ //Brutto, Ezequiel Andrés.
 function ComenzarIngreso () 
 {
  	//definicion de variables
- 	var edadIngresada;
- 	var sexoIngresado;
- 	var estadoCivilIngresado;
+ 	var age;
+ 	var gender;
+ 	var maritalStatus;
+ 	var grossSalary;
+ 	var fileNumber;
+ 	var nationality;
+ 	var message;
 
+ 	message = "Gracias por llenar el formulario,\n verifique que sus datos sean correctos \n para ingresarlos a la base de datos.";
 
+ 	age = prompt("Hola!!! Ingrese su edad:");
+ 	age = parseInt(age);
+    while(isNaN(age) == true || age < 18 || age > 90)
+ 	{
+ 		age = prompt("Por favor, ingrese correctamente su edad:");
+ 		age = parseInt(age);
+ 	}
+    age = "Edad: " + age + " AÑOS.";
 
+ 	gender = prompt("Por favor, indique su sexo.\n (F)EMENINO o (M)ASCULINO.");
+    while(gender != "F" && gender != "M")
+	{
+		gender = prompt("Por favor, indique correctamente su sexo \n (F)EMENINO o (M)ASCULINO.");
+	}
+	if(gender == "F")
+	{
+		gender = "Sexo: FEMENINO.";
+	}
+	else
+	{
+		gender = "Sexo: MASCULINO.";
+	}
+
+	maritalStatus = prompt("Ahora, indique su estado civil:\n (1) SOLTERO/A, (2)CASADO/A, (3)DIVORCIADO/A o (4)VIUDO/A. ");
+	maritalStatus = parseInt(maritalStatus);
+    while(isNaN(maritalStatus) == true || maritalStatus < 1 || maritalStatus > 4)
+	{
+		maritalStatus = prompt("Por favor, ingrese correctamente su estado civil:\n (1) SOLTERO/A, (2)CASADO/A, (3)DIVORCIADO/A o (4)VIUDO/A. ");
+	    maritalStatus = parseInt(maritalStatus);
+	}
+    switch(maritalStatus)
+	{
+		case 1: 
+		    maritalStatus = "Estado civil: SOLTERO/A.";
+		    break;
+		case 2:
+		    maritalStatus = "Estado civil: CASADO/A.";
+		    break;
+		case 3:
+		    maritalStatus = "Estado civil: DIVORCIADO/A.";
+		    break;
+		case 4:
+		    maritalStatus = "Estado civil: VIUDO/A.";
+		    break;
+	}
+
+	grossSalary = prompt("¿Cuál es su sueldo bruto?\n (recuerde que no debe ser menor a $8000.-)");
+	grossSalary = parseInt(grossSalary);
+    while(isNaN(grossSalary) == true || grossSalary < 8000)
+    {
+    	grossSalary = prompt("Por favor, ingrese correctamente su sueldo bruto\n (no menor a $8000.-)");
+	    grossSalary = parseInt(grossSalary);
+    }
+    grossSalary = "Sueldo bruto: $ " + grossSalary + ".-";
+
+    fileNumber = prompt("Ahora, ingrese su N° de Legajo:");
+    fileNumber = parseInt(fileNumber);
+    while(isNaN(fileNumber) == true || fileNumber < 1000 || fileNumber >9999)
+    {
+    	fileNumber = prompt("Por favor, ingrese correctamente su N° de Legajo:");
+        fileNumber = parseInt(fileNumber);
+    }
+    fileNumber = "Su N° de Legajo es: " + fileNumber + ".";
+
+    nationality = prompt("Por último, díganos su nacionalidad:\n (A)RGENTINA, (E)XTRANJERA, o (N)ACIONALIZADO/A.");
+    while(nationality != "A" && nationality != "E" && nationality != "N")
+	{
+		nationality = prompt("Por favor, indique correctamente su nacionalidad: \n (A)RGENTINA, (E)XTRANJERA, o (N)ACIONALIZADO/A.");
+	}
+	if(nationality == "A")
+	{
+		nationality = "Nacionalidad: ARGENTINA.";
+	}
+	else
+	{
+		if (nationality == "E") 
+		{
+			nationality = "Nacionalidad: (E)XTRANJERA.";
+		}
+		else
+		{
+			nationality = "Nacionalidad: (N)ACIONALIZADO/A.";
+		}
+	}
+
+	alert(message);
+
+	document.getElementById('txtIdEdad').value = age;
+	document.getElementById('txtIdSexo').value = gender;
+	document.getElementById('txtIdEstadoCivil').value = maritalStatus;
+	document.getElementById('txtIdSueldo').value = grossSalary;
+	document.getElementById('txtIdLegajo').value = fileNumber;
+	document.getElementById('txtIdNacionalidad').value = nationality;
 }
